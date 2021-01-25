436adf06be03d76d22677616b3a39cf2ff0edbf8
pratikshakarande <77971325+pratikshakarande@users.noreply.github.com>
2021-01-25 18:08:36 +0530

    Add files via upload

---
+++ Assignment 1-Copy1.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Python program to display all the prime numbers within an interval
+
+lower = 0
+upper = 1000
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+            
+
+
+# In[3]:
+
+
+#Write a Python Program for factorial of a number.
+
+num = 7
+
+factorial = 1
+
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+
+
+# In[4]:
+
+
+#Find the sum of n numbers by using the while loop
+
+# Sum of natural numbers up to num
+
+num = 16
+
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   # use while loop to iterate until zero
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
+
+
+# In[ ]:
+
+
+
+
